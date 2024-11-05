2072cbd8a1c09d7aad9f399bc3346cc3b22d1139
Baose162002 <btran438@gmail.com>
2024-11-05 21:55:10 +0700

    Mark_And_MarkDetail

--- FAP/app/src/main/java/com/example/fap/ui/mark/MarkFragment.java
+++ FAP/app/src/main/java/com/example/fap/ui/mark/MarkFragment.java
@@ -7,26 +7,60 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.fap.R;
 import com.example.fap.databinding.FragmentMarkBinding;
 
-public class MarkFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MarkFragment extends Fragment implements MarkAdapter.OnMarkClickListener {
     private FragmentMarkBinding binding;
+    private MarkAdapter adapter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        MarkViewModel markViewModel =
-                new ViewModelProvider(this).get(MarkViewModel.class);
-
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentMarkBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        return binding.getRoot();
+    }
 
-        final TextView textView = binding.textMark;
-        markViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setupRecyclerView();
+        loadSampleData();
+    }
+
+    private void setupRecyclerView() {
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        adapter = new MarkAdapter(new ArrayList<>(), this);
+        binding.recyclerView.setAdapter(adapter);
+    }
+
+    private void loadSampleData() {
+        List<Mark> marks = new ArrayList<>();
+        marks.add(new Mark("EXE201", "Experiential Entrepreneurship 2", "SE1714", 9.2, true));
+        marks.add(new Mark("PRM392", "Mobile Programming", "SE1714", 9.0, true));
+        adapter.setMarks(marks);
+    }
+
+    @Override
+    public void onMarkClick(Mark mark) {
+        Bundle bundle = new Bundle();
+        bundle.putString("courseCode", mark.getCourseCode());
+        bundle.putString("courseName", mark.getCourseName());
+        bundle.putString("className", mark.getClassName());
+        bundle.putFloat("average", (float) mark.getAverage());
+
+        NavController navController = Navigation.findNavController(requireView());
+        navController.navigate(R.id.action_markFragment_to_markDetailFragment, bundle);
     }
 
     @Override
